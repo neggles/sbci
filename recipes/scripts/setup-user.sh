b70b210137d2af67ce55f4cf00f4086683866cbe
Andrew Powers-Holmes <aholmes@omnom.net>
2022-11-15 22:50:20 +1100

    debos: setup-user: add to groups one by one

--- recipes/scripts/setup-user.sh
+++ recipes/scripts/setup-user.sh
@@ -5,8 +5,10 @@ username=${1:?'username is required'}
 password=${2:?'password is required'}
 uid=${3:-1000}
 
-adduser --gecos admin --disabled-password --shell /bin/bash --uid ${uid} ${username}
-adduser ${username} sudo,video,render,audio,bluetooth,plugdev,input,dialout
+adduser --gecos ${username} --disabled-password --shell /bin/bash --uid ${uid} ${username}
+for i in sudo video render audio bluetooth plugdev input dialout; do
+    adduser ${username} $i
+done
 
 # set password and expire it to force a change on first login
 echo "${username}:${password}" | chpasswd
