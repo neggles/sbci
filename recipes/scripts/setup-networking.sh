3d2f2308289d02061696bce3cc137b523e2f9598
Andrew Powers-Holmes <aholmes@omnom.net>
2022-11-16 20:10:44 +1100

    everything: rework of image setup and overlays

--- recipes/scripts/setup-networking.sh
+++ recipes/scripts/setup-networking.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
-
 hostname=${1:?'hostname is required'}
 
 echo "$hostname" > /etc/hostname
-echo -e "127.0.0.1\tlocalhost\t$hostname" > /etc/hosts
-echo -e "127.0.1.1\t$hostname" >> /etc/hosts
+# Generate /etc/hosts
+cat <<EOF > /etc/hosts
+127.0.0.1       localhost
+127.0.1.1       ${hostname}
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+EOF
 
 # Network management
 systemctl enable systemd-networkd
 # DNS resolving
 systemctl enable systemd-resolved
+
+# netplan (if installed)
+command -v netplan && netplan apply || true
--- recipes/scripts/setup-u-boot.sh
+++ recipes/scripts/setup-u-boot.sh
@@ -3,7 +3,30 @@
 # It will generate /etc/default/u-boot, and then run u-boot-update.
 set -e
 
-boardname=${1:?'board name is required'}
+devicetree=${1:?'devicetree is required'}
+arch=${2:-'arm64'}
+
+# rootfs UUID maps from https://uapi-group.org/specifications/specs/discoverable_partitions_specification
+case "${arch}" in
+    arm64|aarch64)
+        rootfs_uuid='b921b045-1df0-41c3-af44-4c6f280d3fae'
+    ;;
+    arm|armhf|aarch32)
+        rootfs_uuid='69dad710-2ce4-4e3c-b16c-21a1d49abed3'
+    ;;
+    riscv64|rv64)
+        rootfs_uuid='72ec70a6-cf74-40e6-bd49-4bda08e8f224'
+    ;;
+    riscv32|rv32)
+        rootfs_uuid='60d5a7fe-8e7d-435c-b714-3dd8162144e1'
+    ;;
+    x64|x86_64|amd64)
+        rootfs_uuid='4f68bce3-e8cd-4db1-96e7-fbcaf984b709'
+    ;;
+    loongarch64)
+        rootfs_uuid='77055800-792c-4f94-b39a-98c91b762bb6'
+    ;;
+esac
 
 # Generate /etc/default/u-boot
 cat <<EOF > /etc/default/u-boot
@@ -16,9 +39,9 @@ U_BOOT_DEFAULT="l0"
 U_BOOT_ENTRIES="all"
 U_BOOT_MENU_LABEL="debian't GNU/Linux"
 U_BOOT_PARAMETERS="earlycon=uart8250,mmio32,0xfe660000 console=ttyS2,1500000n8 rw rootwait audit=0 nosplash loglevel=8"
-U_BOOT_ROOT="root=LABEL=ROOTFS"
+U_BOOT_ROOT="root=UUID=${rootfs_uuid}"
 U_BOOT_TIMEOUT="30"
-U_BOOT_FDT="rockchip/${boardname}.dtb"
+U_BOOT_FDT="${devicetree}"
 U_BOOT_FDT_DIR="/usr/lib/linux-image-"
 #U_BOOT_FDT_OVERLAYS=""
 #U_BOOT_FDT_OVERLAYS_DIR="/boot/dtbo/"
